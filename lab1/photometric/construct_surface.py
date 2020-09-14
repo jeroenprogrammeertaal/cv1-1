f47e25d2038c838799930ad385422a9d9e4c25dc
jeroentaal <jeroen1749@gmail.com>
2020-09-14 18:58:12 +0200

    added row-major and average

--- lab1/photometric/construct_surface.py
+++ lab1/photometric/construct_surface.py
@@ -28,18 +28,37 @@ def construct_surface(p, q, path_type='column'):
         %       height_value = previous_height_value + corresponding_p_value
         
         """
+        for idx in range(1, h):
+            height_map[idx, 0] = height_map[idx-1, 0] + q[idx, 0]
+
+        for row_idx in range(h):
+            for col_idx in range(1, w):
+                height_map[row_idx, col_idx] = height_map[row_idx, col_idx - 1] + p[row_idx, col_idx]            
+
     elif path_type=='row':
         """
         ================
         Your code here
         ================
         """
+        for idx in range(1, w):
+            height_map[0, idx] = height_map[0, idx - 1] + p[0, idx]
+
+        for col_idx in range(w):
+            for row_idx in range(1, h):
+                height_map[row_idx, col_idx] = height_map[row_idx - 1, col_idx] + q[row_idx, col_idx]
+
     elif path_type=='average':
         """
         ================
         Your code here
         ================
         """
+        col_path = construct_surface(p, q, path_type='column')
+        row_path = construct_surface(p, q, path_type='row')
+
+        height_map = (col_path + row_path) / 2
+
         
     return height_map
         
